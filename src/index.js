f389de7a748f7ea76d15b387e0941412c038b741
richesh27 <kunwarrichesh@gmail.com>
2023-05-29 14:10:13 +0530

    Added twitter and Hashtag model v1

--- src/index.js
+++ src/index.js
@@ -4,32 +4,12 @@ const connect =  require('./config/database');
 
 const app = express();
 
-const TweetRepository = require('./repository/tweet-repository');
-const tweetRepo = new TweetRepository();
-
-const Comment = require('./models/comment');
 
 app.listen(3000, async ()=>{
     console.log("Server started at Port");
     await connect();
     console.log("mongoDB connected")
 
-    // const tweet = await tweetRepo.create({
-    //     content :'My tweet',
-    //     userEmail : "m@t.com"
-    // });
-    // const comment = await Comment.create({
-    //     content :'My comment',
-    // });
-    // tweet.comments.push(comment)
-    // await tweet.save();
-    // console.log(tweet)
-    // const tweets =  await Tweet.find({userEmail: 'a@b.com'});
-    // const tweet = await tweetRepo.getWithComments('647300db19b11e0da5398ff4')
-    
-    // const tweet = await tweetRepo.getAll(5,4)
-    // console.log(tweet);
-    // console.log(tweet[2].contentWithEmail)
 
     
 })
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -3,23 +3,26 @@ const mongoose = require ('mongoose');
 const tweetSchema = new mongoose.Schema({
     content : {
         type: String,
-        required: true
+        required: true,
+        max : [250, 'Tweet cannot have more than 250 characters']
     },
-    userEmail : {
-        type: String
-    },
-    comments : [
+    hashtags: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "Comment"
+            ref : 'Hashtag'
         }
     ]
 }, {timestamps: true});
 
 //virtuals in mongoDB
-tweetSchema.virtual('contentWithEmail').get( function process () {
-    return `${this.content} \nCreated by: ${this.userEmail}`
-})
+// tweetSchema.virtual('contentWithEmail').get( function process () {
+//     return `${this.content} \nCreated by: ${this.userEmail}`
+// })
+
+// tweetSchema.pre('save', function (next) {
+//     console.log("inside a hook (middleware ")       // Generally they are called triggers which are used to trigger an event 
+//     next(); //pointing to next middleware          // or after an event like before creation of a query and after creation of query
+// })                                             
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 
